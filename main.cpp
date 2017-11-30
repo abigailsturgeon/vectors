463746a13d771ac4d46bdcb8c077c4c166eef62b
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-11-30 12:20:12 -0700

    Vectors and MAPs

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include "Person.h"
+#include "Resource.h"
+#include "accum.h"
 
 using namespace std;
 
 int main()
 {
     vector<int> v;
-    v.push_back(3);
+    v.push_back(3);     // Add a member
     v.push_back(99);
     v.push_back(-23);
     v.push_back(6);
 
     // cout << v[0] << endl;
 
+    // Using auto iterator with a range
+    // System figures out type
     for(auto i = v.begin(); i != v.end(); i++)      // Auto figures out the type
     {
         cout << *i << " ";
@@ -55,6 +61,42 @@ int main()
     result = min_element(begin(v), end(v));
     cout << "min value is " << *result << endl;
 
+    // Vector of objects
+    vector<Person> vp;
+    Person Waldo("Waldo", "Weber", 123);
+    Person Vandy("Vandy", "Vanderbilt", 678);
+    vp.push_back(Waldo);
+    vp.push_back(Vandy);
 
+    // Use iterator to get objects
+    for(auto ip = vp.begin(); ip != vp.end(); ip++)
+    {
+        cout << *ip << endl;
+    }
+    cout << Waldo << endl;
+
+    // Now let's talk about MAPs
+    map<int, Person> people;
+
+    //      key<int>        = value<Person>
+    people[Waldo.getArNum()] = Waldo;
+    // If the key exist, you update the value
+    // Else you create the value
+    people[Vandy.getArNum()] = Vandy;
+
+    for(auto ip = people.begin(); ip != people.end(); ip++)
+    {
+        // The two components of my map are:
+        // key->first                       value->second
+        cout << ip->first << " " << ip->second.getFirstName();
+    }
+    Person Elmo("Elmo", "Street", 100);
+    people[123] = Elmo;
+    for(auto item:people)
+    {
+        // The two components of my map are:
+        // key->first                       value->second
+        cout << item.first << " " << item.second.getFirstName();
+    }
     return 0;
 }
